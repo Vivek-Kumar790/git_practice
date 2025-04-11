d33521bbd3fc3af4f30a1a1dcbb0cfee98090df8
Vivek-Kumar790 <vivekkumar@wingmanpartners.com>
2025-04-11 17:26:52 +0530

    add multiply feature

--- calci.java
+++ calci.java
@@ -9,10 +9,14 @@ class calci{
     {
         return a-b;
     }
+      public static int multiplyTwoNumber(int a,int b)
+    {
+        return a*b;
+    }
     public static void main(String arg[])
     {
         Scanner sc =new Scanner(System.in);
-        System.out.println("press 1 for addition 2 for subraction");
+        System.out.println("press 1 for addition 2 for subraction 3 for multiplication");
         int choice = sc.nextInt();
         System.out.println("Enter two numbers");
         int a=sc.nextInt();
@@ -21,6 +25,8 @@ class calci{
             case 1 : System.out.println(addTwoNumber(a,b));
             break;
             case 2 : System.out.println(subtractTwoNumber(a,b));
+            break;
+              case 3 : System.out.println(multiplyTwoNumber(a,b));
             break;
         }
         
